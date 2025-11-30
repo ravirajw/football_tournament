265b477f064709b4904e201be3e22d75538182e1
Raviraj Wadhwa <w.raviraj@yahoo.com>
2025-11-30 14:04:57 +0530

    Bump version to v2.3.8

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@
 const CONFIG = {
     // Storage
     STORAGE_KEY: 'footballTournaments',
-    APP_VERSION: 'v2.3.7',
+    APP_VERSION: 'v2.3.8',
     IS_TESTING: false,
     SUPER_USER_CODES: ['SUPER1', 'GOAL2', 'KICK3', 'PASS4', 'WIN5'],
     
